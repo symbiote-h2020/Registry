8065d696b2503a29ccb15dfbc49bb1457e647d0b
Mateusz Łukaszenko <mateusz.lukaszenko@man.poznan.pl>
2017-01-23 10:58:20 +0100

    tests not implemented

--- src/test/java/eu/h2020/symbiote/TestRepositoryManager.java
+++ src/test/java/eu/h2020/symbiote/TestRepositoryManager.java
@@ -1,26 +1,11 @@
 package eu.h2020.symbiote;
 
-import eu.h2020.symbiote.messaging.RabbitManager;
-import eu.h2020.symbiote.model.Platform;
-import eu.h2020.symbiote.repository.PlatformRepository;
-import eu.h2020.symbiote.repository.RepositoryManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.net.MalformedURLException;
-
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 /**
  * Created by mateuszl on 10.01.2017.
  */
 public class TestRepositoryManager {
 
-    private RepositoryManager repositoryManager;
+/*    private RepositoryManager repositoryManager;
     private PlatformRepository mockedPlatformRepo;
     private Platform platformToSave;
     private RabbitManager rabbitManager;
@@ -54,5 +39,5 @@ public class TestRepositoryManager {
 
     @After
     public void tearDown() {
-    }
+    }*/
 }
