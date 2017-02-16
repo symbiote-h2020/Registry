af534e119c8598351a8f9ef0096093ebb98bb4a4
Mateusz Łukaszenko <mateusz.lukaszenko@man.poznan.pl>
2017-02-16 13:36:27 +0100

    tests update

--- src/test/java/eu/h2020/symbiote/TestPlatformRepository.java
+++ src/test/java/eu/h2020/symbiote/TestPlatformRepository.java
@@ -1,22 +1,25 @@
 package eu.h2020.symbiote;
 
 import eu.h2020.symbiote.repository.PlatformRepository;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by mateuszl on 11.01.2017.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={RegistryApplication.class})
+@SpringBootTest({"eureka.client.enabled=false"})
 public class TestPlatformRepository {
-    PlatformRepository repo;
 
-    @Before
-    public void setUp(){
-        repo = Mockito.mock(PlatformRepository.class);
-    }
+    @Autowired
+    private PlatformRepository repo;
 
     @Test
     public void testRepo() {
