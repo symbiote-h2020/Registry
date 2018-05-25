5221193a0b6dcf13ce8279bf23e413af172f0c6d
Mateusz Łukaszenko <mateuszl@man.poznan.pl>
2018-05-25 10:04:34 +0200

    modification request Resources IDs check updated

--- src/main/java/eu/h2020/symbiote/messaging/consumers/resource/ResourceModificationRequestConsumer.java
+++ src/main/java/eu/h2020/symbiote/messaging/consumers/resource/ResourceModificationRequestConsumer.java
@@ -15,14 +15,15 @@ import eu.h2020.symbiote.managers.RabbitManager;
 import eu.h2020.symbiote.managers.RepositoryManager;
 import eu.h2020.symbiote.model.AuthorizationResult;
 import eu.h2020.symbiote.model.RegistryOperationType;
-import eu.h2020.symbiote.model.cim.*;
+import eu.h2020.symbiote.model.cim.Device;
+import eu.h2020.symbiote.model.cim.Resource;
+import eu.h2020.symbiote.model.cim.Service;
 import eu.h2020.symbiote.security.accesspolicies.common.IAccessPolicySpecifier;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpStatus;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -117,8 +118,7 @@ public class ResourceModificationRequestConsumer extends DefaultConsumer {
             //contact with Semantic Manager accordingly to Type of object Description received
             switch (request.getDescriptionType()) {
                 case RDF:
-                    log.info("Message to Semantic Manager Sent. Content Type : RDF. Request: " + request.getBody());
-                    //sending RDF content to Semantic Manager and passing responsibility to another consumer
+                    log.info("Message to Semantic Manager Sent. Content Type : RDF. Request: " + request.getBody());    //sending RDF content to Semantic Manager and passing responsibility to another consumer
                     rabbitManager.sendResourceRdfValidationRpcMessage(this, properties, envelope,
                             message,
                             request.getPlatformId(),
@@ -127,14 +127,7 @@ public class ResourceModificationRequestConsumer extends DefaultConsumer {
                             this.policiesMap);
                     break;
                 case BASIC:
-                    if (checkIfResourcesHaveNullOrEmptyId(request)) {
-                        log.info(request.getBody());
-                        log.error("One of the resources has no ID or list with resources is invalid. Resources not modified!");
-                        response.setStatus(HttpStatus.SC_BAD_REQUEST);
-                        response.setMessage("One of the resources has no ID or list with resources is invalid. Resources not modified!");
-                        rabbitManager.sendRPCReplyMessage(this, properties, envelope,
-                                mapper.writeValueAsString(response));
-                    } else {
+                    if (checkIfEveryResourceHasId(request)) {                                                           //if all of the resources have an Id, request is passed do SM
                         log.info("Message to Semantic Manager Sent. Content Type : BASIC. Request: " + request.getBody());
                         //sending JSON content to Semantic Manager and passing responsibility to another consumer
                         rabbitManager.sendResourceJsonTranslationRpcMessage(this, properties, envelope,
@@ -144,6 +137,13 @@ public class ResourceModificationRequestConsumer extends DefaultConsumer {
                                 authorizationManager,
                                 this.policiesMap,
                                 request.getBody());
+                    } else {                                                                                            //if any of the resources does not have an ID, request is rejected.
+                        log.info(request.getBody());
+                        log.error("One of the resources has no ID or list with resources is invalid. Resources not modified!");
+                        response.setStatus(HttpStatus.SC_BAD_REQUEST);
+                        response.setMessage("One of the resources has no ID or list with resources is invalid. Resources not modified!");
+                        rabbitManager.sendRPCReplyMessage(this, properties, envelope,
+                                mapper.writeValueAsString(response));
                     }
                     break;
             }
@@ -157,50 +157,32 @@ public class ResourceModificationRequestConsumer extends DefaultConsumer {
     }
 
     /**
-     * Checks if given request consists of resources, which all have an ID.
-     *
-     * @param request
-     * @return true if any of given resources does not have an ID.
+     * @param request with resources to check
+     * @return True if every of resources in list has an id. False if any of resources does not have an id.
      */
-    private boolean checkIfResourcesHaveNullOrEmptyId(CoreResourceRegistryRequest request) {
-        Map<String, Resource> resourceMap = new HashMap<>();
-        try {
-            resourceMap = mapper.readValue(request.getBody(), new TypeReference<Map<String, Resource>>() {
-            });
-        } catch (IOException e) {
-            log.error("Could not deserialize content of request!" + e);
-        }
-        List<Resource> resources = resourceMap.values().stream().collect(Collectors.toList());
-        return checkIfEveryResourceHasId(resources);
-    }
-
-    /**
-     * @param resources list with resources to check
-     * @return True if every of resources in list has an id. False if any of resources has not an id.
-     */
-    private boolean checkIfEveryResourceHasId(List<Resource> resources) {
+    private boolean checkIfEveryResourceHasId(CoreResourceRegistryRequest request) {
+        List<Resource> resources = retrieveResourcesListFromRequest(request);
         try {
             for (Resource resource : resources) {
-                if (!checkIfResourceHasId(resource)) {
+
+                if (!checkIfResourceHasId(resource)) {                                                                  //if given resource does not have an id, false will be returned
                     log.error("One of resources does not have an id!");
                     return false;
                 }
-                List<Service> services = new ArrayList<>();
+
                 if (resource instanceof Device) {
-                    services = ((Device) resource).getServices();
-                } else if (resource instanceof MobileSensor) {
-                    services = ((MobileSensor) resource).getServices();
-                } else if (resource instanceof Actuator) {
-                    services = ((Actuator) resource).getServices();
-                }
-                if (!services.isEmpty()) {
-                    for (Service service : services) {
-                        if (!checkIfResourceHasId(service)) {
-                            log.error("One of services does not have an id!");
-                            return false;
+                    List<Service> services = ((Device) resource).getServices();                                                       //if given Resource is a subtype of Device, it can have services inside
+
+                    if (services != null && !services.isEmpty()) {
+                        for (Service service : services) {
+                            if (!checkIfResourceHasId(service)) {                                                           //if given service does not have an id, false will be returned
+                                log.error("One of services does not have an id!");
+                                return false;
+                            }
                         }
                     }
                 }
+
             }
         } catch (Exception e) {
             log.error(e);
@@ -211,13 +193,32 @@ public class ResourceModificationRequestConsumer extends DefaultConsumer {
 
     /**
      * @param resource
-     * @return True if resource has an ID.
+     * @return True if resource has an ID. False if it has not an ID or ID is empty.
      */
     private boolean checkIfResourceHasId(Resource resource) {
-        if (resource.getId() != null && !resource.getId().isEmpty()) {
+        if (resource.getId() == null || resource.getId().isEmpty()) {
+            return false;
+        } else {
+            log.error("Resource (or actuating service) has an ID!");
             return true;
         }
-        log.error("One of the resources (or actuating services) has ID field empty or null!");
-        return false;
     }
+
+    /**
+     * Extracts Resource List from the request
+     *
+     * @param request
+     * @return list with Resource objects
+     */
+    private List<Resource> retrieveResourcesListFromRequest(CoreResourceRegistryRequest request) {
+        Map<String, Resource> resourceMap = new HashMap<>();
+        try {
+            resourceMap = mapper.readValue(request.getBody(), new TypeReference<Map<String, Resource>>() {
+            });
+        } catch (IOException e) {
+            log.error("Could not deserialize content of request!" + e);
+        }
+        return resourceMap.values().stream().collect(Collectors.toList());
+    }
+
 }
